7cc8d5a5c99fb601569585b01a1cc733e4e87769
polat türk <polatturr52@gmail.com>
2024-09-08 17:08:49 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrtakCalisma_RP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
